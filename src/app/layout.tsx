663d5023ed79859e49c65242abdc6c803569f4f8
emma <eoziegbe2@gmail.com>
2023-09-22 22:36:34 -1200

    navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Navbar from '@/components/layouts/navbar';
 import './globals.css'
 import type { Metadata } from 'next'
 import { Montserrat } from "next/font/google";
@@ -16,7 +17,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={montserrat.className}>{children}</body>
+      <body className={montserrat.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
--- src/utils/const.tsx
+++ src/utils/const.tsx
@@ -1,7 +1,25 @@
 import React from 'react'
 
-export default function Const() {
-  return (
-    <div>Const</div>
-  )
-}
+export const NAVLINKS = [
+  {
+      name: 'about', href: '/about' ,
+      title : 'About Us',
+  },
+  {
+      name : 'contact', href: '/contact',
+      title : 'Contact',
+  },
+  {
+      name : 'gallery', href: '/gallery',
+      title : 'Gallery',
+  },
+  {
+      name : 'pricing', href: '/pricing',
+      title : 'Pricing',
+  },
+  {
+      name : 'contact developer', href: '/developer',
+      title : 'Contact Developer',
+  },
+
+]
